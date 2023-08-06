a34b049f97b67dd8088eca4e3d4ab6ebdd7d8e3e
Seven <graf.lancelot7@gmail.com>
2023-08-06 21:33:20 +0300

    add practice

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { ThemeProvider } from 'styled-components';
+// import { GlobalStyle } from './components/GlobalStyle';
+// import { Example1 } from './components/Example1';
+// import { Reader } from './components/Reader/Reader';
+// import publications from './publications.json';
+// import {App} from './components/App';
+// const theme = {};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <Example1/> */}
+     {/* <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Reader items={publications} />
+    </ThemeProvider>  */}
+  </React.StrictMode>
+);
