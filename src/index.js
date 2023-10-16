11af45bb3a8deb9ad6a056c14928c10f0d2daff3
Seven <graf.lancelot7@gmail.com>
2023-10-16 15:07:39 +0300

    add redux with repeta

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { ThemeProvider } from 'styled-components';
+import { App } from 'components/App';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const theme = {
+  colors: {
+    black: '#212121',
+    white: '#fff',
+    red: 'red',
+    green: 'green',
+    orange: 'orange',
+  },
+  radii: {
+    sm: '4px',
+    md: '8px',
+    lg: '16px',
+  },
+  spacing: value => `${value * 4}px`,
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
